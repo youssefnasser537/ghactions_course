82b8e2e651981f9c3d61e262c44c306ac7da55c1
youssef <youssefnasser537@gmail.com>
2025-07-27 18:48:21 +0300

    scheduled task

--- app.js
+++
@@ -1,8 +0,0 @@
-function add(a, b) {
-  return a + b;
-}
-function greet(name) {
-  return 'hello, joo'
-}
-
-module.exports = add;
--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Running test..."
-
-node -e "const add = require('./app'); if (add(2, 3) !== 5) { console.error('❌ Test failed'); process.exit(1); } else { console.log('✅ Test passed'); }"
